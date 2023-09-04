b2f78347019c0cd41314573d54de2d1030b2ae03
Andreas J. Reichel <webmaster@6th-dimension.com>
2023-09-04 15:02:00 +0200

    Make SDL2_mixer depend on --enable-floppy-sound option
    Signed-off-by: Andreas J. Reichel <webmaster@6th-dimension.com>

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include "config.h"
 
 int main(int argc, char **argv)
 {
+#ifdef CONFIG_FLOPPY_SOUND
+        printf("Floppy seek sound enabled\n");
+#endif
         printf("Hello world\n");
         return 0;
 }
